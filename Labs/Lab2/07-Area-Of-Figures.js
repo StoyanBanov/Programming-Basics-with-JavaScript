920cb24c15298d8dde5d259ff8a07c7c460f5bec
Stoyan <s_banov@abv.bg>
2022-10-05 13:42:49 +0300

    lab2-  05 n 06

---
+++ Labs/Lab2/07-Area-Of-Figures.js
@@ -0,0 +1,23 @@
+function area(input){
+    let figureType = input[0]
+    let area
+    if(figureType == "square"){
+        let a = Number(input[1])
+        area = a * a
+    }
+    else if(figureType == "rectangle"){
+        let a = Number(input[1])
+        let b = Number(input[2])
+        area = a * b 
+    }
+    else if(figureType == "circle"){
+        let r = Number(input[1])
+        area = Math.PI * r * r
+    }
+    else if(figureType == "triangle"){
+        let a = Number(input[1])
+        let ha = Number(input[2])
+        area = a * ha / 2
+    }
+    console.log(area.toFixed(3))
+}
