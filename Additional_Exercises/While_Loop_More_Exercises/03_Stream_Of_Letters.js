bf0fc98116b6e012a14eaba9817a6d366536a552
Stoyan <s_banov@abv.bg>
2022-10-12 15:21:48 +0300

    additional exercise with while 03

---
+++ Additional_Exercises/While_Loop_More_Exercises/03_Stream_Of_Letters.js
@@ -0,0 +1,54 @@
+function hiddenMessage(params) {
+    let i = 0
+    let message = ""
+    let word = ""
+    let foundC = false
+    let foundO = false
+    let foundN = false
+    while(true){
+        let symbol = params[i]
+
+        if(foundC && foundO && foundN){
+            message += word + " "
+            word = ""
+            foundC = false
+            foundO = false
+            foundN = false
+        }
+
+        if(symbol === "End"){
+            console.log(message)
+            break
+        }
+        i++
+
+        if(symbol == "c" && foundC == false){
+            foundC = true;
+            continue
+        }
+        else if(symbol == "o" && foundO == false){
+            foundO = true
+            continue
+        }
+        else if(symbol == "n" && foundN == false){
+            foundN = true
+            continue
+        }
+
+        if((symbol.charCodeAt(0) >= 65 && symbol.charCodeAt(0) <= 90) || (symbol.charCodeAt(0) >= 97 && symbol.charCodeAt(0) <= 122)){
+            word += symbol
+        }
+        /*
+        for (let j = "A".charCodeAt(0); j <= "z".charCodeAt(0); j++){
+            if(symbol == String.fromCharCode(j)){
+                message += symbol
+                break
+            }
+            if(j == 90){
+                j = 97
+            }
+        }*/
+    }
+}
+
+hiddenMessage(["H", "n", "e", "l", "l", "o", "o", "c", "t", "c", "h", "o", "e", "r", "e", "n", "e", "End"])
